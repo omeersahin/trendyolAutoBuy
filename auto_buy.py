1b83727975f486f5af10d725bf02df0d9d7aacba
omeersahin <omeeersahin@gmail.com>
2022-10-31 19:21:29 +0300

    XPATH v3

--- auto_buy.py
+++ auto_buy.py
@@ -1,3 +1,4 @@
+from itertools import product
 import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -22,22 +23,12 @@ def webOptions():
 def go_product_page(path):
 
     try:
-        product     = driver.find_element(By.XPATH,path)
-        product_URL  = product.get_attribute("href")    # Ürün sayfasının URL'ini verir.
-        driver.get(product_URL)# Sayfaya gider.
-        #                                                               ------
-        # /html/body/div[1]/div[5]/main/div/div[2]/div[1]/div[2]/div[2]/div[5]/button
-        #                                                               ------ 
-        # Ürün sayfaları değişiklik gösterebildiğinden dolayı bir 5-7 arasından bulunmaktadır.																	  
-        add_to_basket = driver.find_element(By.XPATH,"/html/body/div[1]/div[5]/main/div/div[2]/div[1]/div[2]/div[2]/div[5]/button")
+
+        driver.get(path)# Sayfaya gider.																  
+        add_to_basket = driver.find_element(By.XPATH,"//button[@class='add-to-basket']")
+        add_to_basket.click()   # Sepete ekler.
     except:
-        print("There is not add to basket button at div[5] ")
-        try:
-            add_to_basket = driver.find_element(By.XPATH,"/html/body/div[1]/div[5]/main/div/div[2]/div[1]/div[2]/div[2]/div[6]/button")
-        except:
-            print("There is not add to basket button at div[6] ")
-            add_to_basket = driver.find_element(By.XPATH,"/html/body/div[1]/div[5]/main/div/div[2]/div[1]/div[2]/div[2]/div[7]/button")                                        
-    add_to_basket.click()# Sepete ekleme butonuna tıklar.
+        print("There is not add to basket button")
 
 # Ürün ekleme fonksiyon bloğu.
 # Bazı elementleri Class ismi bazılarını Xpath ile bulmaktadır.								 																	
@@ -45,63 +36,45 @@ def add_product(product_name):
     print("Trying to buy " + product_name + " keyword")
     try:
         driver.get('https://www.trendyol.com/sr?q='+str(product_name)+'&qt='+str(product_name)+'&st='+str(product_name)+'&os=1')
-        driver.find_element(By.XPATH,'/html/body/div/div/div/div[3]/div[1]/a').get_attribute("title")
-        print('Hata Sayfası')
-        return
-    except :
-        try:
+        if driver.find_element(By.XPATH,'//*[@id="search-app"]').get_attribute('id') == 'search-app':
             driver.get('https://www.trendyol.com/sr?q='+str(product_name)+'&qt='+str(product_name)+'&st='+str(product_name)+'&os=1&fc=true&sst=MOST_RATED')
             time.sleep(0.5)
             try:
                 # Bazı sayfalarda ürün tanıtımı yapıldığı için overlay üzerine bir tıklama yapılması gerekebilmektedir.
                 overlay = driver.find_element(By.CLASS_NAME,"overlay")
                 overlay.click()
+                time.sleep(0.5)
                 # ---------------------------------------------------------------
             except:
                 print("Overlay Element Not Found")
 
-            time.sleep(0.5)
-            index = 2
-            while True:
-                try:
-                    add_to_basket = driver.find_element(By.XPATH,"/html/body/div[1]/div[3]/div[2]/div[2]/div/div/div/div[1]/div[2]/div["+str(index)+"]/div[1]/div/div[2]/div[1]/div[2]/button")                                                    
-                    add_to_basket.click()
-                    break
-                except:
-                    if index == 5:
-                        print("There is no Add to Basket Button" )
-                        try:
-                            go_product_page("/html/body/div[1]/div[3]/div[2]/div[2]/div/div/div/div[1]/div[2]/div[5]/div[1]/div/div[2]/div[1]/a")
-                        except:
-                            print("Product URL not found at div[5]")
-                            go_product_page("/html/body/div[1]/div[3]/div[2]/div[2]/div/div/div/div[1]/div[2]/div[4]/div[1]/div/div[2]/div[1]/a")
-                        break
-                    else: 
-                        index+=1
-                        pass      
-        except:
-            print("There is no such element")
-
-
+            #Product Container
+            try:
+                add_to_basket = driver.find_element(By.XPATH,"//button[@class='add-to-basket-button']")
+                add_to_basket.click()    # Sepete ekler.
+            except:
+                print("There is no Add to Basket Button")
+                product = driver.find_element (By.XPATH,"//div[@class='image-container']") # Ürünün resim kapsayıcını bulur.
+                product_URL = product.find_element(By.XPATH,"..").get_attribute("href") # Parent elementin href URL'ini alır.
+                go_product_page(product_URL)# Ürün sayfasına gider.
+    except :
+        print("There is no such element")
         
-
-        
-
-
+    return
 
 if __name__ == "__main__":
     # Chromium ayarları yapılarak ilk çalıştırıldığı blok.
     chrome_options = webOptions()
     driver = webdriver.Chrome(options=chrome_options)
 	# ----------------------------------------------------								   
-    # Excelden okunan anahtar kelimeler sırasıyla ürün ekleme işlemi yapılmaktadır.
-    # En sonunda da sepet kısmına gidilmektedir.	
+    # Excelden okunan anahtar kelimeler sırasıyla ürün ekleme işlemi yapılır.
+    # En sonunda da sepet kısmına gider.	
     products = excel_read()
     for i in products:
-        if ''.join(i).find('-') != -1:
+        if ''.join(i).find('---') != -1:
             add_product('--------')
         else:
-            add_product(''.join(i))
+            add_product(''.join(i)) # '(birlestirilmesi istenen char)'.join((birlesecek ogeler)) ->> '.'.join(['ab', 'pq', 'rs']) -> 'ab.pq.rs'
     time.sleep(2)
     driver.get('https://www.trendyol.com/sepet')
 	# -----------------------------------------------------------------------------	
